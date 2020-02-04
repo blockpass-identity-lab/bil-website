1b5176f8ff0ec17f8b3152405f8c4e8f633b9690
Leonidas <lzoidis123@gmail.com>
2020-02-04 10:39:21 +0000

    Leo's branch

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,5 @@
 import React from "react"
 
-export default () => <div>Bil-Website First Commit (Roddy Branch)</div>
+export default () => (
+  <div>Bil-Website First Commit (Roddy Branch, Leonidas Zoidis)</div>
+)
