9e072d5cd0b7b5cdfcc9122ae0d33429c1225d56
Martin Gras <martin.gras95@gmail.com>
2020-02-11 10:32:47 +0000

    Jumbotron created

---
+++ src/components/jumbotron.js
@@ -0,0 +1,8 @@
+import React from "react"
+import { Jumbotron } from "react-bootstrap"
+
+export default ({children}) => (
+    <Jumbotron>
+        <h1>Most recent events will be displayed here maybe :)</h1>
+    </Jumbotron>
+)
