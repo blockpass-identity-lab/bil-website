2b6cb0a9f1006809e2abaa595d211835cbc931d0
Martin Gras <martin.gras95@gmail.com>
2020-02-24 14:10:05 +0000

    events commented

--- src/components/events.js
+++ src/components/events.js
@@ -2,25 +2,25 @@ import React from "react"
 
 const Events = () => {
   return (
-    <div class="container-fluid bg-light">
-    <div class="row">
-      <div class="col-md-11 offset-1 p-5">
-        <h1>Our Latest Events</h1>
-        <p class="font-weight-light">lorem ipsum dolor sit amet</p>
-      </div>
-    </div>
-    <div class="container-fluid bg-light">
-      <div class="row">
-        <div class="col-md-6 col-sm-12">
-          <h3 class="text-md-center">Latest Events</h3>
-        </div>
-        <div class="col-md-6 col-sm-12">
-          <h3 class="text-md-center">Calendar Events</h3>
-        </div>
-      </div>
-    </div>
-  </div>
- 
+  //   <div class="container-fluid bg-light">
+  //   <div class="row">
+  //     <div class="col-md-11 offset-1 p-5">
+  //       <h1>Our Latest Events</h1>
+  //       <p class="font-weight-light">lorem ipsum dolor sit amet</p>
+  //     </div>
+  //   </div>
+  //   <div class="container-fluid bg-light">
+  //     <div class="row">
+  //       <div class="col-md-6 col-sm-12">
+  //         <h3 class="text-md-center">Latest Events</h3>
+  //       </div>
+  //       <div class="col-md-6 col-sm-12">
+  //         <h3 class="text-md-center">Calendar Events</h3>
+  //       </div>
+  //     </div>
+  //   </div>
+  // </div>
+  <p>Hello</p>
   )
 }
 
