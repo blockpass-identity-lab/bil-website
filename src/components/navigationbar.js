52c27adcd273bb20392fec269acb31b370802ce5
Martin Gras <martin.gras95@gmail.com>
2020-02-24 16:34:19 +0000

    navbar commented out, netlify has an issue with bootstrap navbar

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -4,27 +4,28 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap"
 
 const Navigationbar = () => {
   return (
-    <Navbar bg="light-1" expand="lg">
-    <Navbar.Brand href="/">Blockpass Identity Lab</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto mr-5">
-        <Nav.Link href="/">Home</Nav.Link>
-        <Nav.Link href="/">About</Nav.Link>
-        <Nav.Link href="/">Team</Nav.Link>
-        <Nav.Link href="/">Events</Nav.Link>
-        <Nav.Link href="/">Contact</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="/">Action</NavDropdown.Item>
-          <NavDropdown.Item href="/">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="/">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="/">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  //   <Navbar bg="light-1" expand="lg">
+  //   <Navbar.Brand href="/">Blockpass Identity Lab</Navbar.Brand>
+  //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  //   <Navbar.Collapse id="basic-navbar-nav">
+  //     <Nav className="ml-auto mr-5">
+  //       <Nav.Link href="/">Home</Nav.Link>
+  //       <Nav.Link href="/">About</Nav.Link>
+  //       <Nav.Link href="/">Team</Nav.Link>
+  //       <Nav.Link href="/">Events</Nav.Link>
+  //       <Nav.Link href="/">Contact</Nav.Link>
+  //       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+  //         <NavDropdown.Item href="/">Action</NavDropdown.Item>
+  //         <NavDropdown.Item href="/">Another action</NavDropdown.Item>
+  //         <NavDropdown.Item href="/">Something</NavDropdown.Item>
+  //         <NavDropdown.Divider />
+  //         <NavDropdown.Item href="/">Separated link</NavDropdown.Item>
+  //       </NavDropdown>
+  //     </Nav>
+  //   </Navbar.Collapse>
+  // </Navbar>
 
+  <p>this will be the navbar</p>
   
   )
 }
