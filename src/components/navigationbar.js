8efe2fbf20aef4dfb61ed64d2d341fb834059201
Martin Gras <martin.gras95@gmail.com>
2020-02-24 16:16:08 +0000

    worktribe section comments deleted

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -4,26 +4,28 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap"
 
 const Navigationbar = () => {
   return (
-    <Navbar bg="light-1" expand="lg">
-    <Navbar.Brand href="#home">Blockpass Identity Lab</Navbar.Brand>
-    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-    <Navbar.Collapse id="basic-navbar-nav">
-      <Nav className="ml-auto mr-5">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">About</Nav.Link>
-        <Nav.Link href="#link2">Team</Nav.Link>
-        <Nav.Link href="#link3">Events</Nav.Link>
-        <Nav.Link href="#link4">Contact</Nav.Link>
-        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-          <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-        </NavDropdown>
-      </Nav>
-    </Navbar.Collapse>
-  </Navbar>
+  //   <Navbar bg="light-1" expand="lg">
+  //   <Navbar.Brand href="#home">Blockpass Identity Lab</Navbar.Brand>
+  //   <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  //   <Navbar.Collapse id="basic-navbar-nav">
+  //     <Nav className="ml-auto mr-5">
+  //       <Nav.Link href="#home">Home</Nav.Link>
+  //       <Nav.Link href="#link">About</Nav.Link>
+  //       <Nav.Link href="#link2">Team</Nav.Link>
+  //       <Nav.Link href="#link3">Events</Nav.Link>
+  //       <Nav.Link href="#link4">Contact</Nav.Link>
+  //       <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+  //         <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+  //         <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+  //         <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+  //         <NavDropdown.Divider />
+  //         <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+  //       </NavDropdown>
+  //     </Nav>
+  //   </Navbar.Collapse>
+  // </Navbar>
+  <p>hello world
+  </p>
   
   )
 }
