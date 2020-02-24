c8961c07ac1f82d7b1d7a73709946290e96f2c54
Martin Gras <martin.gras95@gmail.com>
2020-02-24 14:12:11 +0000

    footer comments removed

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,19 +4,19 @@ import React from "react"
 
 const Footer = () => {
   return (
-  //   <div class="card mt-5">
-  //   <div class="card-header">Footer</div>
-  //   <div class="card-body">
-  //     <h5 class="card-title">Special title treatment</h5>
-  //     <p class="card-text">
-  //       With supporting text below as a natural lead-in to additional content.
-  //     </p>
-  //     {/* <a href="#" class="btn btn-primary">
-  //       Go somewhere
-  //     </a> */}
-  //   </div>
-  // </div>
-  <p>Hello</p>  
+    <div class="card mt-5">
+    <div class="card-header">Footer</div>
+    <div class="card-body">
+      <h5 class="card-title">Special title treatment</h5>
+      <p class="card-text">
+        With supporting text below as a natural lead-in to additional content.
+      </p>
+      {/* <a href="#" class="btn btn-primary">
+        Go somewhere
+      </a> */}
+    </div>
+  </div>
+  
   )
 }
 
