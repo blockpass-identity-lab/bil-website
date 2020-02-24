15ded86c17380a64f323d1f37c8cfb2fbf56fa55
Martin Gras <martin.gras95@gmail.com>
2020-02-24 16:32:21 +0000

    Navbar empty links populated with root to solve deploy issue

--- src/components/navigationbar.js
+++ src/components/navigationbar.js
@@ -4,28 +4,27 @@ import { Navbar, Nav, NavDropdown } from "react-bootstrap"
 
 const Navigationbar = () => {
   return (
-    <div>
     <Navbar bg="light-1" expand="lg">
-    <Navbar.Brand href="#home">Blockpass Identity Lab</Navbar.Brand>
+    <Navbar.Brand href="/">Blockpass Identity Lab</Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto mr-5">
-        <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">About</Nav.Link>
-        <Nav.Link href="#link2">Team</Nav.Link>
-        <Nav.Link href="#link3">Events</Nav.Link>
-        <Nav.Link href="#link4">Contact</Nav.Link>
+        <Nav.Link href="/">Home</Nav.Link>
+        <Nav.Link href="/">About</Nav.Link>
+        <Nav.Link href="/">Team</Nav.Link>
+        <Nav.Link href="/">Events</Nav.Link>
+        <Nav.Link href="/">Contact</Nav.Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-          <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-          <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+          <NavDropdown.Item href="/">Action</NavDropdown.Item>
+          <NavDropdown.Item href="/">Another action</NavDropdown.Item>
+          <NavDropdown.Item href="/">Something</NavDropdown.Item>
           <NavDropdown.Divider />
-          <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown.Item href="/">Separated link</NavDropdown.Item>
         </NavDropdown>
       </Nav>
     </Navbar.Collapse>
   </Navbar>
-    </div>
+
   
   )
 }
