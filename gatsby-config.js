849baacfb708d9dffb87dd991715d6fb98221051
Martin Gras <martin.gras95@gmail.com>
2020-02-24 17:26:05 +0000

    bil website contentful space linked with development site

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@
 
 module.exports = {
   /* Your site config here */
-
-  
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        spaceId: '9tsw91dqehj4',
+        accessToken: 'ue76suCe3wMvf-G5XYCHNuE3-12kE2N6G63Nc-P8Edk'
+      }
+    }
+  ]
 }
