4eba3cfcfc8fcd913140c6985a458a2490b3451d
Martin Gras <martin.gras95@gmail.com>
2020-02-24 14:13:23 +0000

    jumbo comments removed

--- src/components/jumbo.js
+++ src/components/jumbo.js
@@ -4,16 +4,16 @@ import { Jumbotron,Button } from "react-bootstrap"
 const Jumbo = () => {
         // Jumbotron Which will hold events
         return(
-            // <Jumbotron>
-            // <h1>BlockPass Identity Lab</h1>
-            // <p>
-            //     This space will possibly be later occupied by current / recent events :)
-            // </p>
-            // <p>
-            //     <Button variant="primary">Learn more</Button>
-            // </p>
-            // </Jumbotron>
-            <p>Hello</p>
+            <Jumbotron>
+            <h1>BlockPass Identity Lab</h1>
+            <p>
+                This space will possibly be later occupied by current / recent events :)
+            </p>
+            <p>
+                <Button variant="primary">Learn more</Button>
+            </p>
+            </Jumbotron>
+            
         )
 }
 
