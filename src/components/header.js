921d554cbbc0d04cd76de7d310b6813c076a8ed4
Roddy's Mac <roddycarson@roddys-mbp.home>
2020-03-02 21:21:23 +0000

    "navbar test"

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,10 @@
 import React from "react"
 import "bootstrap/dist/css/bootstrap.min.css"
 
-
 const Header = () => {
-    return (
-        <h1>This is a header.</h1>
-    )
+  return (
+    <h1 className="header">Blockpass Identity Lab Title Headline Text Space</h1>
+  )
 }
 
 export default Header
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,18 @@
-import React from 'react'
+import React from "react"
 
 // importing components
-import Footer from './footer'
-import Navbar from './navigationbar'
+import Footer from "./footer"
+import Navbar from "./navbar"
 
-const Layout = (props) => {
-    return(
-        <div>
-            <Navbar />
-                {/* Rest of content */}
-                { props.children }
-            <Footer />
-        </div>
-    )
+const Layout = props => {
+  return (
+    <div>
+      <Navbar />
+      {/* Rest of content */}
+      {props.children}
+      <Footer />
+    </div>
+  )
 }
 
 export default Layout
