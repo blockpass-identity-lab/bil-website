5509bd63d6bc1ff9f782716d0b56273d865da4f4
Martin Gras <martin.gras95@gmail.com>
2020-03-03 12:15:40 +0000

    event cards created

--- src/components/events.js
+++ src/components/events.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { graphql, useStaticQuery, Link } from 'gatsby'
 
+import eventsStyles from './events.module.scss'
+import Card from 'react-bootstrap/Card'
+
 const Events = () => {
 
   // Query to contentful to get events
@@ -21,22 +24,39 @@ const Events = () => {
   return(
     <div>
       <h1>Events</h1>
-      <ol>
+      <ol className={eventsStyles.events}>
         {/* Grabbing data from the query and iterating through it to display each event as a list item */}
         {data.allContentfulEvent.edges.map((edge) => {
           return (
-            // List item with event
-            <li>
-              {/* link to the event's own page */}
-              <Link to={`/event/${edge.node.slug}`}>
-                <h2>{edge.node.eventTitle}</h2>
-                <p>{edge.node.eventDate}</p>
-              </Link>
-            </li>
+            <Link to={`/event/${edge.node.slug}`}>
+              <Card style={{ width: '18rem' }} className={ eventsStyles.event }>
+                <Card.Img variant="top" src="https://images.unsplash.com/photo-1582807129843-8a00296ccb37?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=768&q=80/100px180" />
+                  <Card.Body>
+                    <Card.Title>{ edge.node.eventTitle }</Card.Title>
+                    <Card.Text>
+                      { edge.node.eventDate }
+                    </Card.Text>
+                  </Card.Body>
+              </Card>
+            </Link>
+
+
+
+
+            // // List item with event
+            // <li className={eventsStyles.event}>
+            //   {/* link to the event's own page */}
+            //   <Link to={`/event/${edge.node.slug}`}>
+            //     <h2>{edge.node.eventTitle}</h2>
+            //     <p>{edge.node.eventDate}</p>
+            //   </Link>
+            // </li>
           )
         })}
       </ol>
     </div>
+
+
   )
 }
 
