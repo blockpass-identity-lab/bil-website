58b31749842cf51d871deba003343031e0569210
Martin Gras <martin.gras95@gmail.com>
2020-03-10 10:46:52 +0000

    team component created

---
+++ src/components/team.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import { graphql, useStaticQuery } from "gatsby"
+
+const Team = () => {
+    <div>
+        <div>
+            <p>Name</p>
+            <em>Job Title</em>
+        </div>
+    </div>
+}
+
+export default Team
