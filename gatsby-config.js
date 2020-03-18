4714695465196cec8609c78fd7ab5bf69aea4b66
Martin Gras <martin.gras95@gmail.com>
2020-03-18 22:17:01 +0000

    further fixes to .env variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,7 +16,7 @@ module.exports = {
       resolve: "gatsby-source-contentful",
       options: {
         spaceId: process.env.CONTENTFUL_SPACE_ID,
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
       },
     },
     {
