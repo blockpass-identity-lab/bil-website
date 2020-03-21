19419a99ff9af72438494c2b656ff8a45e1adfde
Roddy's Mac <roddycarson@roddys-mbp.home>
2020-03-21 19:40:57 +0000

    navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -3,15 +3,15 @@ import { Navbar, Nav } from "react-bootstrap"
 
 const navbar = () => {
   return (
-    <Navbar className="nav navbar-dark sticky-top" bg="light-1" expand="lg">
+    <Navbar className="navbar-dark sticky-top" bg="light-1" expand="lg">
       <Navbar.Brand href="#home">Blockpass Identity Lab</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav class="navbar navbar-default ml-auto" role="navigation">
+        <Nav class="nav navbar ml-auto mr-5" role="navigation">
           <Nav.Link href="#home">Home</Nav.Link>
           <Nav.Link href="#link2">About</Nav.Link>
           <Nav.Link href="#link3">Events</Nav.Link>
-          <Nav.Link href="#Link4">Team</Nav.Link>
+          <Nav.Link href="#link4">Team</Nav.Link>
           <Nav.Link href="#link5">Projects</Nav.Link>
         </Nav>
       </Navbar.Collapse>
