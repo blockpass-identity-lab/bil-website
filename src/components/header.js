fbdf3d2f5e97656d9c90a0dea2f54aa6997c0736
Leonidas <lzoidis123@gmail.com>
2020-03-23 16:48:40 +0000

    front end changes

--- src/components/header.js
+++ src/components/header.js
@@ -3,9 +3,16 @@ import "bootstrap/dist/css/bootstrap.min.css"
 
 const Header = () => {
   return (
-    <h1 className="header">
-      Protecting personal data from online threats with Blockchain technology.
-    </h1>
+    <div class="container-fluid header_container d-flex">
+      <div class="row justify-content-center align-self-center text-center">
+        <div class="col-12 mx-auto">
+          <h1 className="header_title text-center align-middle center">
+            Protecting personal data from online threats with Blockchain
+            technology
+          </h1>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import Events from "../components/events"
 import Twitter from "../components/twitterFeed"
 import Team from "../components/team"
 import Issues from "../components/issues"
+import Carousel from "../components/caoursel"
 
 // export default () => <div>Bil-Website First Commit (Leonidas Zoidis)</div>
 
@@ -13,9 +14,9 @@ export default () => (
   <div class="bg-dark">
     <Layout>
       <Header />
-      <div class="container-fluid mt-0 bg-dark p-5 content">
+      <div class="container-fluid mt-0 bg-light text-dark p-5 content">
         <h2>Blockpass Identity Lab</h2>
-        <p class="font-weight-light desc">
+        <p class="font-weight-normal desc">
           <span class="font-weight-bold">Blockpass Identity Lab </span> is a
           collaboration between Blockpass IDN and Edinburgh Napier University
           which was announced in April 2018. The initial scope of the
@@ -36,6 +37,8 @@ export default () => (
       </div>
       <div className="content">
         <h2 className="headings">Our Team</h2>
+        <Carousel />
+
         <Team />
       </div>
       <div className="content">
